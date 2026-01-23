8c64830ca35b4d696d000805cf6b26afb9312ecf
techsy17 <tech.sy17@gmail.com>
2026-01-23 10:51:51 +0530

    add url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", 
-      "https://willowy-monstera-403963.netlify.app"
+      "https://myblog-website17.netlify.app"
     ]
   })
 );
